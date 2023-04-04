e576e188c797892ecb61bdaaab57419009f1f98b
Razmo <98541763+rodrigo38550@users.noreply.github.com>
2023-04-04 06:33:21 +0200

    Delete Projet directory

--- Projet/limite.js
+++
@@ -1,8 +0,0 @@
-var uploadField = document.getElementById("file");
-
-uploadField.onchange = function() {
-    if(this.files[0].size > 10485760){              ///Verification de la taille du fichier (limiter a 10 Mo)
-       alert("Le fichier est trops volumineux!");
-       this.value = "";
-    };
-};
